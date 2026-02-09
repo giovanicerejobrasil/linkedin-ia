500f0e4f6a33a5e6ba9154a61917bb0dfbf91ff9
Giovani Brasil <giovanicerejobrasil@gmail.com>
2026-02-09 19:14:25 -0300

    adição dos componentes header, hero e upload photo

--- src/app/_components/content.tsx
+++ src/app/_components/content.tsx
@@ -1,9 +1,24 @@
 "use client";
 
+import { Header } from "./header";
+import { Hero } from "./hero";
+import { UploadPhoto } from "./upload-photo";
+
 export function HomeContent() {
   return (
     <>
-      <h1>TESTE</h1>
+      <Header />
+
+      <main className="container mx-auto px-4 py-8">
+        <div className="grid lg:grid-cols-2 gap-8 lg:gap-12 items-center min-h-[calc(100vh-200px)]">
+          <div className="flex items-center justify-center">
+            <Hero />
+          </div>
+          <div className="flex items-center justify-center">
+            <UploadPhoto />
+          </div>
+        </div>
+      </main>
     </>
   );
 }

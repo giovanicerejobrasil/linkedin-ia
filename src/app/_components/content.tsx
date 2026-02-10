444ec6f2854217aaef01837a3cee6b7a8e1261c7
Giovani Brasil <giovanicerejobrasil@gmail.com>
2026-02-09 21:30:42 -0300

    adição dos useStates e handles para o sistema de input de imagem

--- src/app/_components/content.tsx
+++ src/app/_components/content.tsx
@@ -1,10 +1,27 @@
 "use client";
 
+import { useState } from "react";
 import { Header } from "./header";
 import { Hero } from "./hero";
 import { UploadPhoto } from "./upload-photo";
 
 export function HomeContent() {
+  const [selectedPhoto, setSelectedPhoto] = useState<string | null>(null);
+  const [generatedPhoto, setGeneratedPhoto] = useState<string | null>(null);
+
+  const handleSelectPhoto = (photo: string) => {
+    setSelectedPhoto(photo || null);
+  };
+
+  const handleContinue = (url: string) => {
+    setGeneratedPhoto(url || null);
+  };
+
+  const handleReset = () => {
+    setSelectedPhoto(null);
+    setGeneratedPhoto(null);
+  };
+
   return (
     <>
       <Header />
@@ -15,7 +32,11 @@ export function HomeContent() {
             <Hero />
           </div>
           <div className="flex items-center justify-center">
-            <UploadPhoto />
+            <UploadPhoto
+              onPhotoSelected={handleSelectPhoto}
+              onContinue={handleContinue}
+              selectedPhoto={selectedPhoto}
+            />
           </div>
         </div>
       </main>
